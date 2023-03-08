3b925a3e07da951d744b97822b8dad4db0499946
Vladislav Chudaev <vladislav.chudaev1998@gmail.com>
2023-03-08 18:53:48 +0300

    currency check

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,9 +1,12 @@
 package ru.netology.rest;
 
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
 
 class MobileBankApiTestV4 {
     @Test
@@ -19,6 +22,8 @@ class MobileBankApiTestV4 {
       .then()
           .statusCode(200)
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+              .body("[2].currency", equalTo("RUB"))
+              .body("[1].currency", equalTo("USD"))
       ;
     }
 }
