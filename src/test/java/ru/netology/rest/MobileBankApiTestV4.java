84480aac7f9f405320d69ce75f6e25826c23d20a
Vladislav Chudaev <vladislav.chudaev1998@gmail.com>
2023-04-11 23:31:28 +0300

    fix json schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -11,19 +11,18 @@ import static org.hamcrest.Matchers.equalTo;
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-              .body("[2].currency", equalTo("RUB"))
-              .body("[1].currency", equalTo("USD"))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+        ;
     }
 }
